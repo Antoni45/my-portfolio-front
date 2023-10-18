cf34fffd78906af22a13899323a9158b25f508a7
Nirina Antoni Ndremanjary <antoni@nomade.mg>
2023-10-18 16:47:46 +0300

    first commit

---
+++ src/components/head/Header.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import './header.css'
+
+const Header: FC = () => {
+  // JSX
+  return (
+    <div className="root">
+      <h4>Head</h4>
+    </div>
+  );
+};
+
+export default Header;
